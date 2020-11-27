f094c34777c7e3f65ed9644b69813816a950bbf8
Alix <ducrosalix@hotmail.fr>
2020-11-27 14:59:38 +0100

    Add fading animation

--- zenifier.js
+++ zenifier.js
@@ -1,61 +1,32 @@
+let hideSentences = ['🌟 Instant & Free academic success 🌟', '🏆 Boost my h-index 🏆', '✨ Beautify this page ✨']
+let showSentences = ['😩 Let\'s feel miserable 😩', '⛔️ Do not click this ⛔️', '💥 I like it when it hurts 💥']
+
 let button = document.createElement('button')
 button.classList.add('zen-button', 'notzen')
-button.innerHTML = '🌟 Instant & Free academic success 🌟'
-button.style = 
-`
-border-radius: 15px;
-position: fixed;
-top: 10px;
-right: 10px;
-background-color: white;
-color: green;
-border-style: inset;
-border-color: green;
-padding: 5px;
-`
+button.innerHTML = hideSentences[Math.floor(Math.random() * hideSentences.length)]
 
 button.addEventListener('click', () => {
     if (button.classList.contains('notzen')) {
         for (row of document.querySelectorAll('tr[role=row]')) {
             if (row.innerHTML.toUpperCase().includes('NOT ACCEPTED')) {
-                row.style = 'visibility: collapse'
+                //row.style = 'visibility: collapse'
+                row.classList.add('hidden')
             }
         }
 
-        button.innerHTML = '😩 Let\'s feel miserable 😩'
-        button.style = `
-        border-radius: 15px;
-        position: fixed;
-        top: 10px;
-        right: 10px;
-        background-color: white;
-        color: red;
-        border-style: inset;
-        border-color: red;
-        padding: 5px;
-        `
+        button.innerHTML = showSentences[Math.floor(Math.random() * showSentences.length)]
 
         button.classList.toggle('notzen')
     } else {
 
         for (row of document.querySelectorAll('tr[role=row]')) {
             if (row.innerHTML.toUpperCase().includes('NOT ACCEPTED')) {
-                row.style = 'visibility: visible'
+                //row.style = 'visibility: visible'
+                row.classList.remove('hidden')
             }
         }
 
-        button.innerHTML = '🌟 Instant & Free academic success 🌟'
-        button.style = `
-        border-radius: 15px;
-        position: fixed;
-        top: 10px;
-        right: 10px;
-        background-color: white;
-        color: green;
-        border-style: inset;
-        border-color: green;
-        padding: 5px;
-        `
+        button.innerHTML = hideSentences[Math.floor(Math.random() * hideSentences.length)]
 
         button.classList.toggle('notzen')
     }
