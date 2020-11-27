f01fb9246fa70f322d0a482f41e47a86c216e4c3
Alix <ducrosalix@hotmail.fr>
2020-11-27 13:43:32 +0100

    initial commit

---
+++ zenifier.js
@@ -0,0 +1,65 @@
+let button = document.createElement('button')
+button.classList.add('zen-button', 'notzen')
+button.innerHTML = '🌟 Instant & Free academic success 🌟'
+button.style = 
+`
+border-radius: 15px;
+position: fixed;
+top: 10px;
+right: 10px;
+background-color: white;
+color: green;
+border-style: inset;
+border-color: green;
+padding: 5px;
+`
+
+button.addEventListener('click', () => {
+    if (button.classList.contains('notzen')) {
+        for (row of document.querySelectorAll('tr[role=row]')) {
+            if (row.innerHTML.includes('not accepted')) {
+                row.style = 'visibility: collapse'
+            }
+        }
+
+        button.innerHTML = '😩 Let\'s feel miserable 😩'
+        button.style = `
+        border-radius: 15px;
+        position: fixed;
+        top: 10px;
+        right: 10px;
+        background-color: white;
+        color: red;
+        border-style: inset;
+        border-color: red;
+        padding: 5px;
+        `
+
+        button.classList.toggle('notzen')
+    } else {
+        for (row of document.querySelectorAll('tr[role=row]')) {
+            if (row.innerHTML.includes('not accepted')) {
+                row.style = 'visibility: visible'
+            }
+        }
+
+        button.innerHTML = '🌟 Instant & Free academic success 🌟'
+        button.style = `
+        border-radius: 15px;
+        position: fixed;
+        top: 10px;
+        right: 10px;
+        background-color: white;
+        color: green;
+        border-style: inset;
+        border-color: green;
+        padding: 5px;
+        `
+
+        button.classList.toggle('notzen')
+    }
+})
+
+
+
+document.querySelector('body').append(button)
